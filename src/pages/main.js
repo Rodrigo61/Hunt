6ae04ec8330d28b36c52a28c5de9ccc586cced48
Rodrigo Amaral <rodrigoaf61@gmail.com>
2018-11-08 14:35:24 -0200

    Criado WebView em product.js
    Criado um componente stateless, product.js, para abrir uma webview que renderiza o website indicado pela URL de cada um dos produtos consumidos pela nossa aplicação oriundos da API externa em nodejs.

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,8 +6,50 @@ export default class Main extends Component {
     title: "JSHunt"
   };
 
-  render()
-  {
+  state = {
+    productInfo: {},
+    docs: [],
+    page: 1,
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get('/products?page='+page);
+
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ docs: [...this.state.docs, ...docs], productInfo , page});
+  };
+
+  loadMore = () => {
+    const {page, productInfo} = this.state;
+    
+    if (page != productInfo.pages) {
+      this.loadProducts(page + 1);
+    }
+
+  };
+
+  renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}> {item.title} </Text>
+      <Text style={styles.productText}> {item.description} </Text>
+
+      <TouchableOpacity
+        style={styles.productButton}
+        onPress={() => {
+          this.props.navigation.navigate('Products', {product: item});
+        }}
+      >
+        <Text style={styles.productButtonText}>Acessar URL</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  render() {
     return (
       <View>
         <Text> Página Main </Text>
